c5f529830cedd5b89742505e5c4bfe914068c65f
Antony.Huart <antony.huart37@gmail.com>
2023-04-26 18:59:17 +0200

    problem fix of adding multiple task

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
-const templateDeleteBtns  = document.querySelectorAll('.delete'); 
-const templateCheckboxs   = document.querySelectorAll('.check');
+//let templateCheckboxs   = document.querySelectorAll('.check');
 
 const radioBtns = document.querySelectorAll('fieldset > input');
 const newTask   = document.querySelector('.add-new-task');
 const addButton = document.querySelector('.add-button');
 const taskList  = document.querySelector('.container-task-list > ul');
-const checkbox  = createElements('checkbox', 'input');
-const li        = createElements('li', 'li');
-const deleteBtn = createElements('deleteBtn', 'button');
-const bin       = createElements('bin', 'img');
 
 
 /**
@@ -39,7 +34,11 @@ function createElements(name, tag) {
  * created and set all elements inside the li(newTask)
  * @returns {HTMLElement}
  */
-function ReadyElements() {
+function readyElements() {
+  const li        = createElements('li', 'li');
+  const checkbox  = createElements('checkbox', 'input');
+  const deleteBtn = createElements('deleteBtn', 'button');
+  const bin       = createElements('bin', 'img');
   setElement(checkbox, 'type', 'checkbox');
   setElement(checkbox, 'class', 'check');
   setElement(deleteBtn, 'class', 'delete');
@@ -47,23 +46,27 @@ function ReadyElements() {
   setElement(bin, 'alt', 'bin');
   deleteBtn.append(bin);
   li.append(checkbox, newTask.value, deleteBtn);
+
   return li;
 };
 
+
 function addTask(e) {
   if(newTask.value !== '' && (e.type === 'click' || e.code === 'Enter')) {
-    taskList.prepend(ReadyElements());
+    taskList.prepend(readyElements());
     newTask.value = '';
     newTask.autofocus;
   };
-};
-
-function deleteTask(e) {
-  e.currentTarget.parentNode.remove();
+  
+  const bins = taskList.querySelectorAll('.delete')
+  bins.forEach(bin => {
+    bin.addEventListener('click', deleteTask)
+  });  
 };
 
 function filterTask(e) {
-  templateCheckboxs.forEach(checkbox => {
+  const checkboxs = taskList.querySelectorAll('.check')
+  checkboxs.forEach(checkbox => {
     if(e.currentTarget.id === 'all') {
       checkbox.parentNode.style.display = 'flex';
     };
@@ -82,32 +85,22 @@ function filterTask(e) {
       };
     }
   });
-  if(e.currentTarget.id === 'all') {
-    checkbox.parentNode.style.display = 'flex';
-  };
-  if(e.currentTarget.id === 'to-do') {
-    if (checkbox.checked) {
-      checkbox.parentNode.style.display = 'none';
-    } else {
-      checkbox.parentNode.style.display = 'flex';        
-    };
-  }
-  if(e.currentTarget.id === 'did') {
-    if (!checkbox.checked) {
-      checkbox.parentNode.style.display = 'none';
-    } else {
-      checkbox.parentNode.style.display = 'flex';        
-    };
-  }
+};
+
+function deleteTask(e) {
+  e.currentTarget.parentNode.remove();
 };
 
 addButton.addEventListener('click', addTask);
 newTask.addEventListener('keyup', addTask);
-deleteBtn.addEventListener('click', deleteTask);
-templateDeleteBtns.forEach(deleteBtn => {
-  deleteBtn.addEventListener('click', deleteTask)  
-});
+
 radioBtns.forEach(radioBtn => {
   radioBtn.addEventListener('click', filterTask)
 });
+
+addTask()
+ 
+
+
+
 
